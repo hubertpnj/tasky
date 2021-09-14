3700a1cabbd2a6dc0b504dc0c67909f69e992a53
Hubert Janoszka <HubertPNJ@protonmail.com>
2021-09-14 18:29:51 +0200

    Implemented IntDFSTopoSort

--- src/main/java/com/github/hubertpnj/tasky/sort/IntDFSTopoSort.java
+++ src/main/java/com/github/hubertpnj/tasky/sort/IntDFSTopoSort.java
@@ -1,14 +1,53 @@
 package com.github.hubertpnj.tasky.sort;
 
 import com.github.hubertpnj.tasky.graph.Graph;
+import com.github.hubertpnj.tasky.graph.IntGraph;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 
 // https://en.wikipedia.org/wiki/Topological_sorting#Depth-first_search
 public class IntDFSTopoSort implements TopoSort<Integer> {
 
+    private enum Mark {
+        NONE, TEMPORARY, PERMANENT
+    }
+
+    private void visit(Graph<Integer> graph, final int vertex, List<Integer> vertexes, Mark[] marks) {
+
+        switch (marks[vertex]) {
+            case TEMPORARY:
+                throw new IllegalArgumentException();
+            case PERMANENT:
+                return;
+        }
+
+        marks[vertex] = Mark.TEMPORARY;
+
+        for (int neighbour : graph.getNeighbours(vertex))
+            visit(graph, neighbour, vertexes, marks);
+
+        marks[vertex] = Mark.PERMANENT;
+        vertexes.add(vertex);
+    }
+
     @Override
     public Collection<Integer> sort(Graph<Integer> graph) {
-        return null;
+
+        List<Integer> vertexes = new ArrayList<>(graph.size());
+        Mark[] marks = new Mark[graph.size()];
+
+        Arrays.fill(marks, Mark.NONE);
+
+        for (int vertex : graph) {
+            Collection<Integer> neighbours = graph.getNeighbours(vertex);
+
+            if (!neighbours.isEmpty())
+                visit(graph, vertex, vertexes, marks);
+        }
+
+        return vertexes;
     }
 }
