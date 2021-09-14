eb222c0e39379ad2f53fef4590ee4da70b379e69
Hubert Janoszka <HubertPNJ@protonmail.com>
2021-09-14 18:27:44 +0200

    Changes in Graph

--- src/main/java/com/github/hubertpnj/tasky/graph/Graph.java
+++ src/main/java/com/github/hubertpnj/tasky/graph/Graph.java
@@ -2,12 +2,10 @@ package com.github.hubertpnj.tasky.graph;
 
 import java.util.Collection;
 
-public interface Graph<V> extends Iterable<Graph.Entry<V>> {
-    interface Entry<V> {
-        V getVertex();
-        Collection<V> getNeighbours();
-    }
-
+public interface Graph<V> extends Iterable<V> {
     void addEdge(V vertex1, V vertex2);
+
     Collection<V> getNeighbours(V vertex);
+
+    int size();
 }
