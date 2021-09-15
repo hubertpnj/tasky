1c6a9467951354b642a2e65f7d24f581946cb4ef
Hubert Janoszka <HubertPNJ@protonmail.com>
2021-09-15 22:04:59 +0200

    Implemented IntKahnTopoSort

--- src/main/java/com/github/hubertpnj/tasky/sort/IntKahnTopoSort.java
+++ src/main/java/com/github/hubertpnj/tasky/sort/IntKahnTopoSort.java
@@ -2,13 +2,43 @@ package com.github.hubertpnj.tasky.sort;
 
 import com.github.hubertpnj.tasky.graph.Graph;
 
-import java.util.Collection;
+import java.util.*;
 
-// https://www.geeksforgeeks.org/topological-sorting-indegree-based-solution/
 public class IntKahnTopoSort implements TopoSort<Integer> {
 
     @Override
     public Collection<Integer> sort(Graph<Integer> graph) {
-        return null;
+        int[] degrees = new int[graph.size()];
+
+        for (int vertex : graph)
+            for (int neighbour : graph.getNeighbours(vertex))
+                degrees[neighbour]++;
+
+        Queue<Integer> queue = new ArrayDeque<>();
+
+        for (int vertex : graph)
+            if (degrees[vertex] == 0)
+                queue.add(vertex);
+
+        List<Integer> vertexes = new ArrayList<>();
+
+        int visits = 0;
+
+        while (!queue.isEmpty()) {
+            int vertex = queue.poll();
+
+            vertexes.add(vertex);
+            degrees[vertex]++;
+            visits++;
+
+            for (int neighbour : graph.getNeighbours(vertex))
+                if (--degrees[neighbour] == 0)
+                    queue.add(neighbour);
+        }
+
+        if (visits != graph.size())
+            throw new IllegalArgumentException();
+
+        return vertexes;
     }
 }
